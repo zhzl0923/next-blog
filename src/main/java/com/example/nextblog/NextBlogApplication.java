f62aeda4552689481202d73c25e69e9d100997ec
zzl <188877869@qq.com>
2021-12-18 06:01:45 +0800

    layout

--- src/main/java/com/example/nextblog/NextBlogApplication.java
+++ src/main/java/com/example/nextblog/NextBlogApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NextBlogApplication {
+public class NextBlogApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(NextBlogApplication.class, args);
--- src/main/java/com/example/nextblog/controller/HomeController.java
+++ src/main/java/com/example/nextblog/controller/HomeController.java
@@ -3,13 +3,13 @@ package com.example.nextblog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @ResponseBody
     @GetMapping("/")
-    public String index() {
-        return "hello world";
+    public ModelAndView index() {
+        return new ModelAndView("home");
     }
 }
